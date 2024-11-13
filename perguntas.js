94b0bcb0b4cbc5f041513a896cfa494679d98425
Anally Fernanda <0000110839906xsp@al.educacao.sp.gov.br>
2024-11-13 09:56:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'VELOCIDADE',
+    'MOTO PARA VOCÊ QUE PROCURA VELOCIDADE?',
+    'CBR1000 RR Fireblade – Honda'
+)
+
+criaCartao(
+    'COTIDIANO',
+    'MOTO PARA VOCÊ USAR NO DIA A DIA?',
+    'Honda CG 160'
+)
+
+criaCartao(
+    'Programação',
+    'MOTO PARA VOCÊ QUE GOSTA DE UMA TRILHA?',
+    ' Honda CRF 250F'
+)
+
+criaCartao(
+    'LOJA',
+    'ONDE ENCONTRAR ESSES MODELOS?',
+    'Lojas Honda'
+)
